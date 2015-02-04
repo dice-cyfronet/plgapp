458113567c2489bdc0a1ae4a396b320c7d566c3c
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-02-04 08:44:31 +0100

    session timeout after 40 min on production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_plgapp_session'
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store,
+                                         key: '_plgapp_session',
+                                         expire_after: 40.minutes
+else
+  Rails.application.config.session_store :cookie_store,
+                                         key: '_plgapp_session'
+end
