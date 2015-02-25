7472e2fa6e8bbb1a47c81594f9e748eed9ebf162
Maciej Pawlik <m.pawlik@cyfronet.pl>
2015-02-25 17:42:16 +0100

    Further implementation of job methods.

--- user_apps/test1/plgappjs/rimrock.js
+++ user_apps/test1/plgappjs/rimrock.js
@@ -19,36 +19,81 @@ var Rimrock = function () {
         });
     };
 
-    this.run = function (job) {
+    var extendByFields = function (target, source, fields) {
+        $.each(fields, function (idx, field) {
+            if (source.hasOwnProperty(field)) {
+                target[field] = source[field];
+            }
+        });
+        return target;
+    };
+
+    // /process
+
+    this.run = function (cb, job) {
         getCSRFToken(function (err, token) {
             if (err) {
-                job.onFinished(err);
+                cb(err);
                 return;
             }
             doRequest(token);
         });
 
         var success = function (data, status) {
-            if (job.onFinished != undefined) {
-                job.onFinished(null, data);
+            if (cb != undefined) {
+                cb(null, data);
             }
         };
         var error = function (xhr, status, error) {
-            if (job.onFinished != undefined) {
-                job.onFinished(new Error(status + ' ' + error));
+            if (cb != undefined) {
+                cb(new Error(status + ' ' + error));
             }
         };
 
         var doRequest = function (token) {
+            var data = extendByFields({}, job, ['host', 'command']);
             $.ajax({
                 url: rimrockProxy + '/process',
                 type: 'POST',
                 headers: {'X-CSRF-Token': token},
                 contentType: 'application/json',
-                data: JSON.stringify({
-                    host: job.host,
-                    command: job.command
-                }),
+                data: JSON.stringify(data),
+                success: success,
+                error: error
+            });
+        };
+    };
+
+    // /jobs
+
+    this.submitJob = function (cb, job) {
+        getCSRFToken(function (err, token) {
+            if (err) {
+                cb(err);
+                return;
+            }
+            doRequest(token);
+        });
+
+        var success = function (data, status) {
+            if (cb != undefined) {
+                cb(null, data);
+            }
+        };
+        var error = function (xhr, status, error) {
+            if (cb != undefined) {
+                cb(new Error(status + ' ' + error));
+            }
+        };
+
+        var doRequest = function (token) {
+            var data = extendByFields({}, job, ['host', 'script', 'working_directory', 'tag']);
+            $.ajax({
+                url: rimrockProxy + '/jobs',
+                type: 'POST',
+                headers: {'X-CSRF-Token': token},
+                contentType: 'application/json',
+                data: JSON.stringify(data),
                 success: success,
                 error: error
             });
@@ -81,6 +126,37 @@ var Rimrock = function () {
             success: success,
             error: error
         });
+    };
+
+    this.deleteJob = function (cb, job_id) {
+        getCSRFToken(function (err, token) {
+            if (err) {
+                cb(err);
+                return;
+            }
+            doRequest(token);
+        });
+
+        var success = function (data, status) {
+            if (cb != undefined) {
+                cb(null, data);
+            }
+        };
+        var error = function (xhr, status, error) {
+            if (cb != undefined) {
+                cb(new Error(status + ' ' + error));
+            }
+        };
+
+        var doRequest = function (token) {
+            $.ajax({
+                url: rimrockProxy + '/jobs/' + job_id,
+                type: 'DELETE',
+                headers: {'X-CSRF-Token': token},
+                success: success,
+                error: error
+            });
+        };
     };
 };
 
