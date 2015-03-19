3ff8b4e74d5ddc5173ecca0a7e9d4f91edc9c028
Marek Kasztelnik <mkasztelnik@gmail.com>
2015-03-19 10:38:10 +0100

    Clean dropbox account when no dropbox app

--- app/services/dropbox/delete_service.rb
+++ app/services/dropbox/delete_service.rb
@@ -10,6 +10,7 @@ module Dropbox
     def execute
       delete_path
       app_member.dropbox_entries.destroy_all if app_member
+      clean_dropbox_account if has_dropbox_app?
     end
 
     private
@@ -18,6 +19,17 @@ module Dropbox
       client.file_delete("/#{@subdomain}")
     rescue DropboxError => e
       raise e unless e.http_response.class == Net::HTTPNotFound
+    end
+
+    def has_dropbox_app?
+      author.apps.joins(:app_members).
+        where(app_members: { dropbox_enabled: true }).
+        count
+    end
+
+    def clean_dropbox_account
+      author.update_attributes(dropbox_access_token: nil,
+                               dropbox_user: nil)
     end
   end
 end
--- spec/services/dropbox/delete_service_spec.rb
+++ spec/services/dropbox/delete_service_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Dropbox::DeleteService do
   let(:author) { create(:user) }
   let(:client) { instance_double('DropboxClient') }
 
+  before { allow(client).to receive(:file_delete) }
+
   it 'removes dropbox app directory' do
     expect_delete
 
@@ -21,12 +23,21 @@ RSpec.describe Dropbox::DeleteService do
     it 'removes all dropbox exntries when app exist' do
       dir_entries('dir1', 'dir1/subdir')
       file_entry('file.txt')
-      expect_delete
 
       service.execute
 
       expect(app_member.dropbox_entries.count).to eq 0
     end
+  end
+
+  it 'clean dropbox account when no dropbox app present for the user' do
+    author.update_attributes(dropbox_access_token: 'token', dropbox_user: '123')
+
+    service.execute
+    author.reload
+
+    expect(author.dropbox_access_token).to be_nil
+    expect(author.dropbox_user).to be_nil
   end
 
   def expect_delete
